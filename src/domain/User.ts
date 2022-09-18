15d1b009f960df9d0a7f3cc774e3aad81321f6ea
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-09-18 19:20:27 +0500

    added form validation

---
+++ src/domain/User.ts
@@ -0,0 +1,13 @@
+export interface User {
+  name: string
+  surname: string
+  phone: string
+  email: string
+  isStudent?: boolean
+  isItWorker?: boolean
+  isNotItWorker?: boolean
+  position?: string
+  company?: string
+  stack?: string
+  sphere?: string
+}
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,12 +1,24 @@
+import clsx from 'clsx'
 import Head from 'next/head'
 import { useState } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import PhoneInput from 'react-phone-input-2'
 
+import { User } from '@/domain/User'
+
 const Form = () => {
-  const [isStudent, setIsStudent] = useState(false)
-  const [isItWorker, setIsItWorker] = useState(false)
-  const [isNotItWorker, setIsNotItWorker] = useState(false)
-  const [privacy, setPrivacy] = useState(true)
+  const [privacy, setPrivacy] = useState<boolean>(true)
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm<User>({})
+
+  const onSubmit: SubmitHandler<User> = data => {
+    console.log(data)
+  }
 
   return (
     <>
@@ -18,35 +30,113 @@ const Form = () => {
         style={{ backgroundImage: `url("/photo.webp")` }}
       >
         <div className='hero-overlay bg-opacity-60' />
-        <div className='my-10 flex w-11/12 flex-col gap-6 rounded-xl bg-base-300 p-6 shadow-md md:w-[600px] md:p-10'>
+        <div className='my-10 flex w-11/12 flex-col gap-6 rounded-xl bg-base-300 p-6 shadow-md md:w-[700px] md:p-10'>
           <h1 className='text-3xl'>Регистрация</h1>
-          <form className='flex flex-col gap-5'>
+          <form
+            noValidate={true}
+            className='flex flex-col gap-5'
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className='flex flex-col gap-4 md:flex-row'>
-              <input required className='input w-full' placeholder='Имя' />
-              <input required className='input w-full' placeholder='Фамилия' />
+              <div className='flex w-full flex-col gap-2'>
+                <input
+                  className={clsx('input w-full', errors.name && 'input-error')}
+                  placeholder='Имя'
+                  {...register('name', {
+                    required: 'Обязательное поле',
+                    minLength: {
+                      value: 2,
+                      message: 'Поле должно содержать не менее 2 символов',
+                    },
+                    maxLength: {
+                      value: 30,
+                      message: 'Поле не может содержать более 30 символов',
+                    },
+                  })}
+                />
+                {errors.name && (
+                  <p className='flex items-center gap-2 text-xs text-red-500'>
+                    {errors.name?.message}
+                  </p>
+                )}
+              </div>
+              <div className='flex w-full flex-col gap-2'>
+                <input
+                  className={clsx(
+                    'input w-full',
+                    errors.surname && 'input-error',
+                  )}
+                  placeholder='Фамилия'
+                  {...register('surname', {
+                    required: 'Обязательное поле',
+                    minLength: {
+                      value: 2,
+                      message: 'Поле должно содержать не менее 2 символов',
+                    },
+                    maxLength: {
+                      value: 30,
+                      message: 'Поле не может содержать более 30 символов',
+                    },
+                  })}
+                />
+                {errors.surname && (
+                  <p className='flex items-center gap-2 text-xs text-red-500'>
+                    {errors.surname?.message}
+                  </p>
+                )}
+              </div>
             </div>
-            <PhoneInput
-              country={'ru'}
-              inputClass='input w-full'
-              specialLabel=''
-              placeholder='7 (999) 999 99 99'
-              inputProps={{
-                required: true,
-              }}
-            />
-            <input
-              className='input w-full'
-              placeholder='Email'
-              type='email'
-              required
-            />
+            <div className='flex w-full flex-col gap-2'>
+              <Controller
+                name='phone'
+                control={control}
+                rules={{ required: 'Обязательное поле' }}
+                // @ts-ignore
+                render={({ field }) => (
+                  <PhoneInput
+                    country='ru'
+                    inputClass={clsx(
+                      'input w-full',
+                      errors.phone && 'input-error',
+                    )}
+                    specialLabel=''
+                    placeholder='7 (999) 999 99 99'
+                    {...field}
+                  />
+                )}
+              />
+              {errors.phone && (
+                <p className='flex items-center gap-2 text-xs text-red-500'>
+                  {errors.phone?.message}
+                </p>
+              )}
+            </div>
+            <div className='flex w-full flex-col gap-2'>
+              <input
+                className={clsx('input w-full', errors.email && 'input-error')}
+                placeholder='Email'
+                type='email'
+                {...register('email', {
+                  required: 'Обязательное поле',
+                  pattern: {
+                    value:
+                      /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/,
+                    message: 'Некорректный email',
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className='flex items-center gap-2 text-xs text-red-500'>
+                  {errors.email?.message}
+                </p>
+              )}
+            </div>
             <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
               <label className='flex cursor-pointer items-center gap-4'>
                 <input
                   type='checkbox'
                   className='checkbox-secondary checkbox'
-                  checked={isStudent}
-                  onChange={() => setIsStudent(!isStudent)}
+                  {...register('isStudent')}
                 />
                 <span className='label-text'>Студент</span>
               </label>
@@ -55,8 +145,7 @@ const Form = () => {
                 <input
                   type='checkbox'
                   className='checkbox-secondary checkbox'
-                  checked={isItWorker}
-                  onChange={() => setIsItWorker(!isItWorker)}
+                  {...register('isItWorker')}
                 />
                 <span className='label-text'>Работаю в it</span>
               </label>
@@ -65,39 +154,40 @@ const Form = () => {
                 <input
                   type='checkbox'
                   className='checkbox-secondary checkbox'
-                  checked={isNotItWorker}
-                  onChange={() => setIsNotItWorker(!isNotItWorker)}
+                  {...register('isNotItWorker')}
                 />
                 <span className='label-text'>Работаю не в it</span>
               </label>
             </div>
 
-            {isStudent && <></>}
+            {watch('isStudent') && <></>}
 
-            {isItWorker && (
+            {watch('isItWorker') && (
               <>
                 <textarea
                   className='textarea'
                   placeholder='Кем вы работаете?'
-                  required
+                  {...register('position')}
                 />
                 <textarea
                   className='textarea'
                   placeholder='В какой компании вы работаете?'
+                  {...register('company')}
                 />
                 <textarea
                   className='textarea'
                   placeholder='Какой стек технологий вы используете?'
+                  {...register('stack')}
                 />
               </>
             )}
 
-            {isNotItWorker && (
+            {watch('isNotItWorker') && (
               <>
                 <textarea
                   className='textarea'
                   placeholder='В какой сфере вы работаете?'
-                  required
+                  {...register('sphere')}
                 />
               </>
             )}
