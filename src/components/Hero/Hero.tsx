316e6309740a68e3d56a029ca7a4805814087b56
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-09-20 08:45:08 +0500

    off animation on modile device

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,11 @@
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 
+import useMediaQuery from '@/utils/useMediaQuery'
+
 export const Hero = () => {
   const router = useRouter()
+  const isDesktop = useMediaQuery('(min-width: 768px)')
 
   return (
     <div
@@ -12,98 +15,125 @@ export const Hero = () => {
       <div className='hero-overlay bg-base-300/60'></div>
       <div className='hero-content text-center text-neutral-content'>
         <div className='max-w-full'>
-          <motion.h1
-            initial='offscreen'
-            whileInView='onscreen'
-            viewport={{ once: true }}
-            variants={{
-              offscreen: {
-                y: 300,
-                opacity: 0,
-              },
-              onscreen: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  type: 'spring',
-                  bounce: 0.4,
-                  duration: 0.8,
-                  delay: 0,
+          {isDesktop ? (
+            <motion.h1
+              initial='offscreen'
+              whileInView='onscreen'
+              viewport={{ once: true }}
+              variants={{
+                offscreen: {
+                  y: 300,
+                  opacity: 0,
                 },
-              },
-            }}
-            className='mb-5 text-[48px] font-bold text-white md:text-[84px]'
-          >
-            YAMAl.DEV CONF
-          </motion.h1>
-          <motion.p
-            initial='offscreen'
-            whileInView='onscreen'
-            viewport={{ once: true }}
-            variants={{
-              offscreen: {
-                y: 300,
-                opacity: 0,
-              },
-              onscreen: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  type: 'spring',
-                  bounce: 0.4,
-                  duration: 0.8,
-                  delay: 0.1,
+                onscreen: {
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    type: 'spring',
+                    bounce: 0.4,
+                    duration: 0.8,
+                    delay: 0,
+                  },
                 },
-              },
-            }}
-            className='mb-5 text-[24px] font-bold md:text-[42px]'
-          >
-            IT конференция на Ямале
-          </motion.p>
-          <motion.p
-            initial='offscreen'
-            whileInView='onscreen'
-            viewport={{ once: true }}
-            variants={{
-              offscreen: {
-                y: 300,
-                opacity: 0,
-              },
-              onscreen: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  type: 'spring',
-                  bounce: 0.4,
-                  duration: 0.8,
-                  delay: 0.2,
+              }}
+              className='mb-5 text-[48px] font-bold text-white md:text-[84px]'
+            >
+              YAMAl.DEV CONF
+            </motion.h1>
+          ) : (
+            <h1 className='mb-5 text-[48px] font-bold text-white md:text-[84px]'>
+              YAMAl.DEV CONF
+            </h1>
+          )}
+          {isDesktop ? (
+            <motion.p
+              initial='offscreen'
+              whileInView='onscreen'
+              viewport={{ once: true }}
+              variants={{
+                offscreen: {
+                  y: 300,
+                  opacity: 0,
                 },
-              },
-            }}
-            className='mb-5 text-[14px] md:text-[21px]'
-          >
-            online-offline 5-6 ноября
-          </motion.p>
-          <motion.button
-            initial='offscreen'
-            whileInView='onscreen'
-            viewport={{ once: true }}
-            variants={{
-              offscreen: {
-                opacity: 0,
-              },
-              onscreen: {
-                opacity: 1,
-                transition: {
-                  delay: 0.3,
+                onscreen: {
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    type: 'spring',
+                    bounce: 0.4,
+                    duration: 0.8,
+                    delay: 0.1,
+                  },
                 },
-              },
-            }}
-            className='btn-secondary btn'
-            onClick={() => router.push('/form')}
-          >
-            Регистрация
-          </motion.button>
+              }}
+              className='mb-5 text-[24px] font-bold md:text-[42px]'
+            >
+              IT конференция на Ямале
+            </motion.p>
+          ) : (
+            <p className='mb-5 text-[24px] font-bold md:text-[42px]'>
+              IT конференция на Ямале
+            </p>
+          )}
+          {isDesktop ? (
+            <motion.p
+              initial='offscreen'
+              whileInView='onscreen'
+              viewport={{ once: true }}
+              variants={{
+                offscreen: {
+                  y: 300,
+                  opacity: 0,
+                },
+                onscreen: {
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    type: 'spring',
+                    bounce: 0.4,
+                    duration: 0.8,
+                    delay: 0.2,
+                  },
+                },
+              }}
+              className='mb-5 text-[14px] md:text-[21px]'
+            >
+              online-offline 5-6 ноября
+            </motion.p>
+          ) : (
+            <p className='mb-5 text-[14px] md:text-[21px]'>
+              online-offline 5-6 ноября
+            </p>
+          )}
+          {isDesktop ? (
+            <motion.button
+              initial='offscreen'
+              whileInView='onscreen'
+              viewport={{ once: true }}
+              variants={{
+                offscreen: {
+                  opacity: 0,
+                },
+                onscreen: {
+                  opacity: 1,
+                  transition: {
+                    delay: 0.3,
+                  },
+                },
+              }}
+              className='btn-secondary btn'
+              onClick={() => router.push('/form')}
+            >
+              Регистрация
+            </motion.button>
+          ) : (
+            <button
+              className='btn-secondary btn'
+              onClick={() => router.push('/form')}
+            >
+              Регистрация
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/components/Program/Program.tsx
+++ src/components/Program/Program.tsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion'
 
+import useMediaQuery from '@/utils/useMediaQuery'
+
 const themes = [
   'Frontend',
   'Backend',
@@ -15,53 +17,72 @@ const themes = [
   'Воркшопы',
 ]
 
-export const Program = () => (
-  <div className='flex min-h-screen flex-col items-center justify-center gap-10 bg-gradient-to-t from-secondary to-base-200'>
-    <h2 className='text-2xl text-white lg:text-5xl'>Программа конференции</h2>
-    <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
-      {themes.map((theme, index) => (
-        <motion.div
-          key={theme}
+export const Program = () => {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center gap-10 bg-gradient-to-t from-secondary to-base-200'>
+      <h2 className='text-2xl text-white lg:text-5xl'>Программа конференции</h2>
+      <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+        {themes.map((theme, index) =>
+          isDesktop ? (
+            <motion.div
+              key={theme}
+              initial='offscreen'
+              whileInView='onscreen'
+              viewport={{ once: true }}
+              variants={{
+                offscreen: {
+                  opacity: 0,
+                },
+                onscreen: {
+                  opacity: 1,
+                  transition: {
+                    delay: index / 10,
+                  },
+                },
+              }}
+              className='flex w-[300px]  items-center justify-center rounded-xl p-5 shadow transition-all hover:shadow-none'
+            >
+              {theme}
+            </motion.div>
+          ) : (
+            <div
+              key={theme}
+              className='flex w-[300px]  items-center justify-center rounded-xl p-5 shadow transition-all hover:shadow-none'
+            >
+              {theme}
+            </div>
+          ),
+        )}
+      </div>
+      {isDesktop ? (
+        <motion.p
           initial='offscreen'
           whileInView='onscreen'
           viewport={{ once: true }}
           variants={{
             offscreen: {
+              y: 300,
               opacity: 0,
             },
             onscreen: {
+              y: 0,
               opacity: 1,
               transition: {
-                delay: index / 10,
+                delay: themes.length / 10,
               },
             },
           }}
-          className='flex w-[300px]  items-center justify-center rounded-xl p-5 shadow transition-all hover:shadow-none'
+          className='text-center text-lg text-white lg:text-2xl'
         >
-          {theme}
-        </motion.div>
-      ))}
+          Анонс программы 4 октября
+        </motion.p>
+      ) : (
+        <p className='text-center text-lg text-white lg:text-2xl'>
+          Анонс программы 4 октября
+        </p>
+      )}
     </div>
-    <motion.p
-      initial='offscreen'
-      whileInView='onscreen'
-      viewport={{ once: true }}
-      variants={{
-        offscreen: {
-          y: 300,
-          opacity: 0,
-        },
-        onscreen: {
-          y: 0,
-          opacity: 1,
-          transition: {
-            delay: themes.length / 7.5,
-          },
-        },
-      }}
-      className='text-center text-lg text-white lg:text-2xl'
-    >
-      Анонс программы 4 октября
-    </motion.p>
-  </div>
-)
+  )
+}
