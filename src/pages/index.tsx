1f93325068f2ba1290b2985df1f72d89849f5b72
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-09-20 08:28:49 +0500

    fix hero import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 import { Address } from '@/components/Address'
 import { Footer } from '@/components/Footer'
-import { Hero } from '@/components/Hero/Hero'
+import { Hero } from '@/components/Hero'
 import { Program } from '@/components/Program'
 
 const Home: NextPage = () => (
