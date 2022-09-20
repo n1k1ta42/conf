b640fd913f0041e2351038cde6cacb07595f0190
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-09-20 08:26:05 +0500

    refactoring home page

---
+++ src/components/Address/Address.tsx
@@ -0,0 +1,98 @@
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`
+
+export const Address = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [isInView, setIsInView] = useState(false)
+
+  return (
+    <div className='hero min-h-screen bg-base-300'>
+      <div className='hero-content flex-col gap-20 lg:flex-row'>
+        <motion.a
+          href='https://vk.com/polaris_artresidence'
+          target='_blank'
+          rel='noreferrer'
+          className='order-2 lg:order-1'
+          initial={false}
+          animate={
+            isLoaded && isInView
+              ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+              : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+          }
+          transition={{ duration: 1 }}
+          viewport={{ once: true, amount: 0.8 }}
+          onViewportEnter={() => setIsInView(true)}
+        >
+          <Image
+            src='/polaris.webp'
+            alt='polaris'
+            width={280}
+            height={351}
+            className='max-w-sm rounded-lg shadow-2xl'
+            onLoad={() => setIsLoaded(true)}
+          />
+        </motion.a>
+        <motion.div
+          initial='offscreen'
+          whileInView='onscreen'
+          viewport={{ once: true, amount: 0.8 }}
+          variants={{
+            offscreen: {
+              x: 300,
+              opacity: 0,
+            },
+            onscreen: {
+              x: 0,
+              opacity: 1,
+              transition: {
+                type: 'spring',
+                bounce: 0.4,
+                duration: 0.8,
+                delay: 0.3,
+              },
+            },
+          }}
+          className='order-1 lg:order-2'
+        >
+          <h1 className='text-xl font-bold lg:text-5xl'>
+            Арт-резеденция &quot;Полярис&quot;
+          </h1>
+          <p className='flex gap-1 py-6'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='h-6 w-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
+              />
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
+              />
+            </svg>
+            Адрес: г. Салехард, ул. Чубынина 17
+          </p>
+          <a
+            className='link-hover link'
+            href='https://yandex.ru/maps/-/CCUVVIx~0D'
+            target='_blank'
+            rel='noreferrer'
+          >
+            66.529707, 66.613675
+          </a>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Program/Program.tsx
@@ -0,0 +1,67 @@
+import { motion } from 'framer-motion'
+
+const themes = [
+  'Frontend',
+  'Backend',
+  'data science',
+  'Product design',
+  'Менеджмент',
+  'Agile',
+  'Образование',
+  'HR',
+  'Game dev',
+  'Devops',
+  'Machine learning',
+  'Воркшопы',
+]
+
+export const Program = () => (
+  <div className='flex min-h-screen flex-col items-center justify-center gap-10 bg-gradient-to-t from-secondary to-base-200'>
+    <h2 className='text-2xl text-white lg:text-5xl'>Программа конференции</h2>
+    <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+      {themes.map((theme, index) => (
+        <motion.div
+          key={theme}
+          initial='offscreen'
+          whileInView='onscreen'
+          viewport={{ once: true }}
+          variants={{
+            offscreen: {
+              opacity: 0,
+            },
+            onscreen: {
+              opacity: 1,
+              transition: {
+                delay: index / 10,
+              },
+            },
+          }}
+          className='flex w-[300px]  items-center justify-center rounded-xl p-5 shadow transition-all hover:shadow-none'
+        >
+          {theme}
+        </motion.div>
+      ))}
+    </div>
+    <motion.p
+      initial='offscreen'
+      whileInView='onscreen'
+      viewport={{ once: true }}
+      variants={{
+        offscreen: {
+          y: 300,
+          opacity: 0,
+        },
+        onscreen: {
+          y: 0,
+          opacity: 1,
+          transition: {
+            delay: themes.length / 7.5,
+          },
+        },
+      }}
+      className='text-center text-lg text-white lg:text-2xl'
+    >
+      Анонс программы 4 октября
+    </motion.p>
+  </div>
+)
