a4109828c4ed55c449642987c62da1edb0f78bbb
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-10-13 10:25:21 +0300

    added yandex metric

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 import { Toaster } from 'react-hot-toast'
 
 import { Header } from '@/components/Header'
 
 import '@/styles/globals.css'
+
+import { GTM_ID } from '@/utils/gtm'
 
 import app from '../../package.json'
 
@@ -22,6 +25,20 @@ function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </main>
       <Toaster />
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id='gtag-base'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
     </>
   )
 }
