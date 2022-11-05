d60ff927bd203b0c85d7988bca044a7d3085106b
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-11-05 14:46:18 +0500

    remove iframe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,6 @@ import { Program } from '@/components/Program'
 const Home: NextPage = () => (
   <>
     <Hero />
-    <div className='hero min-h-screen'>
-      <iframe
-        width='860'
-        height='515'
-        src='https://www.youtube.com/embed/-2pBeMzh7H0'
-        title='YouTube video player'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-      ></iframe>
-    </div>
     <Address />
     <Program />
     <Footer />
